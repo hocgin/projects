47a4d4c96bf2d982390e33d1682136d6e908101a
hocgin <hocgin@gmail.com>
2020-10-24 01:20:15 +0800

    Add Gin-Footer

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,12 @@
 import styles from './index.less';
 import React from 'react';
+import GinEggs from '@hocgin/gin-eggs';
 import GinFooter from '@hocgin/gin-footer';
 
 function BasicLayout(props) {
   return (
     <div className={styles.normal}>
+      <GinEggs/>
       <h1 className={styles.title}>hocgin`s Projects</h1>
       <div className={styles.gradientLine}/>
       {props.children}
